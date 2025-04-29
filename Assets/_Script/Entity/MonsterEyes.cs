267d8270cc18507963f99b806d3eef383bfef166
Albert <alik2006sootkill@gmail.com>
2025-04-29 19:46:17 +0700

    update

--- Assets/_Script/Entity/MonsterEyes.cs
+++ Assets/_Script/Entity/MonsterEyes.cs
@@ -1,17 +1,41 @@
 using System;
+using _Script.Manager;
+using _Script.Player;
 using UnityEngine;
+using Zenject;
 
 public class MonsterEyes : MonoBehaviour
 {
+    [Inject] private GameManager _gameManager;
+    
     [SerializeField] private MonsterController _monsterController;
     [SerializeField] private MonsterAi _monsterAi;
-
-    private void OnTriggerEnter(Collider other)
+    [SerializeField] private Transform rayCast;
+    private bool raySeePlayer = false;
+    private void OnTriggerStay(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if(other.CompareTag("Player") && raySeePlayer)
         {
-            // _monsterController.ChangeState(MonsterState.Hunting);
             _monsterAi.Sink(-1,-1,1);
         }
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(rayCast.position, _gameManager.Player.transform.position - rayCast.position, out hit, Vector3.Distance(_gameManager.Player.transform.position, rayCast.position), ~0 ,QueryTriggerInteraction.Ignore))
+        {
+            if (hit.collider.gameObject.TryGetComponent(out PlayerController playerController))
+            {
+                raySeePlayer = true;
+                Debug.DrawRay(rayCast.position, _gameManager.Player.transform.position - rayCast.position, Color.green); 
+            }
+            else
+            {
+                raySeePlayer = false;
+                Debug.DrawRay(rayCast.position, _gameManager.Player.transform.position - rayCast.position, Color.red); 
+
+            }
+        }   
     }
 }
