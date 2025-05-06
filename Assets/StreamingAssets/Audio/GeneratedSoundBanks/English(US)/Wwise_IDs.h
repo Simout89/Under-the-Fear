267b05361aec7ab121ea7652efc02c84209dc485
b3auty4real <b3auty4real@gmail.com>
2025-05-07 02:18:20 +0300

    add 2 busses for music and FX and 2 RTCP for them

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/English(US)/Wwise_IDs.h
@@ -0,0 +1,75 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_FOOTSTEP = 1602358412U;
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+        static const AkUniqueID PLAY_TIRED_BREATH = 1817535745U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace SURFACE
+        {
+            static const AkUniqueID GROUP = 1834394558U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CONCRETE = 841620460U;
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID GRAVIY = 2252896817U;
+                static const AkUniqueID METAL = 2473969246U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace SURFACE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID FOOTSTEP_VOLUME = 511984960U;
+        static const AkUniqueID MUSICINTENSITY = 1301299809U;
+        static const AkUniqueID VOLUME_FX_PLAYER_SETTINGS = 2436629268U;
+        static const AkUniqueID VOLUME_MUSIC_PLAYER_SETTINGS = 1274705203U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID LVL1 = 141331490U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC_BUS = 3127962312U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID BIG_HALL_REV = 1319739747U;
+        static const AkUniqueID CAVE_REV = 1445918944U;
+        static const AkUniqueID CORIDOR_REV = 1389012769U;
+        static const AkUniqueID SMALL_ROOM_REV = 2663941762U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
