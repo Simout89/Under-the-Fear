9c0f201164c9d136e9d65a9efdad5506640d94a9
b3auty4real <b3auty4real@gmail.com>
2025-05-21 05:53:21 +0300

    Add RTCP All_Volume_Player_Settings, NEED TEST! change gear_drop sounds

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,9 +13,26 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_CASSETEPLAYER_BUTTON = 2055319774U;
+        static const AkUniqueID PLAY_DRINKING = 2006984080U;
+        static const AkUniqueID PLAY_FLASHLIGHT_DISABLE = 1652482609U;
+        static const AkUniqueID PLAY_FLASHLIGHT_ENABLE = 1470796794U;
         static const AkUniqueID PLAY_FOOTSTEP = 1602358412U;
+        static const AkUniqueID PLAY_GEAR_DROP = 3106012101U;
+        static const AkUniqueID PLAY_GEAR_PICKUP = 1253718314U;
+        static const AkUniqueID PLAY_GEN1 = 703779141U;
+        static const AkUniqueID PLAY_GEN2 = 703779142U;
+        static const AkUniqueID PLAY_LOW_ROAR = 4157024899U;
+        static const AkUniqueID PLAY_MECHANICAL_SOUND = 2652031523U;
         static const AkUniqueID PLAY_MUSIC = 2932040671U;
+        static const AkUniqueID PLAY_PLAYER_CHARGE_FLASHLIGH = 1015499099U;
+        static const AkUniqueID PLAY_ROAR = 2770252186U;
+        static const AkUniqueID PLAY_SCARY_METALIC_SOUND = 2270754086U;
+        static const AkUniqueID PLAY_SCREAM = 2470536401U;
+        static const AkUniqueID PLAY_STOP_DRINK = 3276893109U;
         static const AkUniqueID PLAY_TIRED_BREATH = 1817535745U;
+        static const AkUniqueID PLAY_WATER_MACHINE = 3039676177U;
+        static const AkUniqueID PLAY_WATER_MACHINE_MAX = 3687303446U;
     } // namespace EVENTS
 
     namespace SWITCHES
@@ -38,8 +55,11 @@ namespace AK
 
     namespace GAME_PARAMETERS
     {
+        static const AkUniqueID ALL_VOLUME_PLAYER_SETTINGS = 545571051U;
         static const AkUniqueID FOOTSTEP_VOLUME = 511984960U;
         static const AkUniqueID MUSICINTENSITY = 1301299809U;
+        static const AkUniqueID VOLUME_FX_PLAYER_SETTINGS = 2436629268U;
+        static const AkUniqueID VOLUME_MUSIC_PLAYER_SETTINGS = 1274705203U;
     } // namespace GAME_PARAMETERS
 
     namespace BANKS
@@ -51,7 +71,16 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC_BUS = 3127962312U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID BIG_HALL_REV = 1319739747U;
+        static const AkUniqueID CAVE_REV = 1445918944U;
+        static const AkUniqueID CORIDOR_REV = 1389012769U;
+        static const AkUniqueID SMALL_ROOM_REV = 2663941762U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
